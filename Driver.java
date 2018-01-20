9acc2428eadf5aa919728fbbbbac4236b6516866
Matthew Noel <31493274+matthewnoel@users.noreply.github.com>
2018-01-20 15:10:20 -0600

    Cleaned Up
    Deleted all the old and temporary files
    Created new Driver.java file

--- Driver.java
+++ Driver.java
@@ -35,7 +35,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
 
-public class StMchTighterTurns extends LinearOpMode {
+public class Driver extends LinearOpMode {
 
         /**
          * Calibrates gyro.
